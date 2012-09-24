bccb2616b5381b60b73771b0562443abeae85051
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2012-09-24 16:35:11 +0300

    create tables

---
+++ sql/DROP ENTU TABLES.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `property`;
+DROP TABLE IF EXISTS `property_definition`;
+DROP TABLE IF EXISTS `relationship`;
+DROP TABLE IF EXISTS `entity`;
+DROP TABLE IF EXISTS `relationship_definition`;
+DROP TABLE IF EXISTS `entity_definition`;
+DROP TABLE IF EXISTS `counter`;
